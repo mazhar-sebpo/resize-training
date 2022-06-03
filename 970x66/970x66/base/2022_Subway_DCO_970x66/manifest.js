3f51190abb58f3cbfb1e8d0776bb12ee9d4aac15
ali-haidar-tauhid <103109207+ali-haidar-tauhid@users.noreply.github.com>
2022-06-03 20:26:13 +0600

    970x66

---
+++ 970x66/970x66/base/2022_Subway_DCO_970x66/manifest.js
@@ -0,0 +1,34 @@
+FT.manifest({
+	"filename": "index.html",
+	"width": 970,
+	"height": 66,
+	"clickTagCount": 1,
+	"hideBrowsers": ["ie8"],
+	"richLoads": [
+        { "name": "main_rl","src": "2022_Subway_970x66_rl" }
+    ],
+	"instantAds": [
+		{ "name": "insertCSS",               			"type": "text",     "default": ""},	
+		{ "name": "frame1_block_hex",           		"type": "text",     "default": "#044923" },
+		{ "name": "frame1_headline_txt",           		"type": "text",     "default": "15% OFF ANY<br><medium>FOOTLONG</medium>" },
+		{ "name": "frame1_headline_size_hex_xy",       	"type": "text",     "default": "27|#ffffff|78,3" },
+		{ "name": "frame1_headlineSVG_img",           	"type": "image",    "default": "images/15offHeadline.svg" },
+		{ "name": "frame1_headlineSVG_xy",           	"type": "text",     "default": "0,-7" },
+		{ "name": "frame1_offerCodeTop_txt",           	"type": "text",     "default": "IN APP WITH CODE" },
+		{ "name": "frame1_offerCodeTop_hex_xy",        	"type": "text",     "default": "18|#044923|408,4" },
+		{ "name": "frame1_offerCodeBottom_txt",        	"type": "text",     "default": "15SUB" },
+		{ "name": "frame1_offerCodeBottom_hex_xy",     	"type": "text",     "default": "40|#044923|426,21" },
+		{ "name": "frame1_product_img",           		"type": "image",    "default": "images/Subway_Image_1020x737.png" },
+		{ "name": "frame1_product_xy",           		"type": "text",     "default": "620,-15" },
+		{ "name": "frame1_cta_txt",           			"type": "text",     "default": "LEARN MORE" },
+		{ "name": "frame1_cta_size_textHex_btnHex_xy",	"type": "text",		"default": "8|#ffffff|#044923|807,36" },
+	    { "name": "frame1_logo_img",          			"type": "image",    "default": "images/970x66_logo.png" },
+        { "name": "frame1_logo_xy",                  	"type": "text",     "default": "0,0" },
+        { "name": "legal_txt",           				"type": "text",     "default": "LEGAL" },
+        { "name": "legal_size_hex_xy",           		"type": "text",     "default": "6|#ffffff|5,54" },
+        { "name": "legalPanel_txt",           			"type": "text",     "default": "Full: Redeemable at participating restaurants for app/online orders only.<br/> Extras additional. 1 use per order. Plus tax. No additional discounts. Expires<br/> XX/XX/XX.<br/><br/><br/>Shortened: Redeemable at select shops for app/online orders. Extras addt'l.<br/> 1 use. Plus tax. No addt’l discounts. Exp XX/XX/XX. <br/><br/><br/>On app: Redeemable at participating restaurants for app/online orders only.<br/> Use code: 15SUB. Extras additional. 1 use per order. Plus tax. No additional<br/> discounts. Expires XX/XX/XX." },
+        { "name": "background_img",           			"type": "image",    "default": "images/blank.png" },
+        { "name": "clicktag1_url",           			"type": "text",     "default": "https://www.subway.com/en-CA/" },
+		{ "name": "main_rl", 							"type": "richload", "default": "2022_Subway_970x66_rl"}
+	]
+});
